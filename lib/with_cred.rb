51dc99e514ec97071ce4b4d35fd6ed656e4e3f38
John Cant <a.johncant@gmail.com>
2013-05-12 22:03:33 +0100

    Now works without Rails

--- lib/with_cred.rb
+++ lib/with_cred.rb
@@ -3,17 +3,39 @@ require "with_cred/version"
 class CredentialsNotFoundError < StandardError ; end
 
 module WithCred
+
+  def self.credentials_dir
+    @@credentials_dir
+  end
+  def self.credentials_dir=(foo)
+    @@credentials_dir = foo
+  end
+
+  if defined?(Rails)
+    def self.credentials_mode
+      Rails.application.config.credentials_mode
+    end
+
+    @@credentials_dir = Rails.root
+  else
+    def self.credentials_mode
+      "production"
+    end
+
+    @@credentials_dir = ENV["PWD"]
+  end
+
   def self.entials_for(file)
 
     # This method ignores the block if the credentials are required, and we are not supposed to need them.
     # Passes the credentials into the block as read from the credentials YAML file at "#{Rails.root}/credentials/#{file}.yaml"
 
-    if (allowed = (Rails.application.config.credentials_mode == "production"))
+    if (allowed = (self.credentials_mode == "production"))
 
       credentials = nil
 
       begin
-        credentials = YAML::load_file("#{Rails.root}/credentials/#{file.to_s}.yaml")
+        credentials = YAML::load_file(File.join(self.credentials_dir, "/credentials/#{file.to_s}.yaml"))
       rescue
         raise CredentialsNotFoundError
       end
